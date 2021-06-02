9c03493c1afb6d12b823313cb242b45ffc194f3a
Roddy Andres Correa <ra.correa90@gmail.com>
2021-06-02 17:34:44 -0500

    update files

--- config.js
+++ config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: { /* don't expose password or any sensitive info, done only for demo */
-    host: env.DB_HOST || 'localhost',
+    host: env.DB_HOST || 'dataparquesdb.crg7rfevyvh3.us-east-1.rds.amazonaws.com',
     user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || 'root',
-    database: env.DB_NAME || 'db',
+    password: env.DB_PASSWORD || 'yKlUgGnZY6cJ',
+    database: env.DB_NAME || 'dataparquesdb',
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
